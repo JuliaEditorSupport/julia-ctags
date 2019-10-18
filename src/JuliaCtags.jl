2961afffc17f7cb0ceaca4ec06c9ff7f1c679870
Mark Wells <maw461@psu.edu>
2019-10-18 14:21:40 -0400

    Update src/JuliaCtags.jl
    Co-Authored-By: Lyndon White <oxinabox@ucc.asn.au>

--- src/JuliaCtags.jl
+++ src/JuliaCtags.jl
@@ -3,7 +3,7 @@ module JuliaCtags
 export get_config
 
 function get_config(destfile = "julia_ctags")
-    sourcefile = (@__DIR__)*"/ctags"
+    sourcefile = joinpath(@__DIR__, "ctags")
     cp(sourcefile, destfile)
 end
 
